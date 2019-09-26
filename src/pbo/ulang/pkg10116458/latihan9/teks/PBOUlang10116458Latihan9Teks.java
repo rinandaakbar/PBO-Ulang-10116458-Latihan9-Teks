55683401639ffbef0aeef9aa5caa099980b9c2c3
nanda <nanda@nanda>
2019-09-26 22:05:20 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan9/teks/PBOUlang10116458Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : Rinanda Al Qorrie Akbar
+ * NIM      : 10116458
+ */
+public class PBOUlang10116458Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
